ceca180822a51dd9721b9085b8934ed7e8d0409b
Kanhai Patel (TSLLP-DEV-3) <kanhaipatel@gudapps.com>
2025-05-15 13:05:13 +0530

    Conversion tracking database changes
    * Left out one file

--- app/Models/TrackedOrder.php
+++ app/Models/TrackedOrder.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrackedOrder extends Model
 {
-    //
+    protected $fillable = [
+        'store_id',
+        'order_id',
+        'order_data',
+    ];
+
+    protected $casts = [
+        'order_data' => 'array',
+    ];
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(TrackedOrderItem::class);
+    }
+
+    public function customer()
+    {
+        return $this->hasOne(TrackedCustomer::class);
+    }
+
+    public function conversionLogs()
+    {
+        return $this->hasMany(ConversionLog::class, 'order_id', 'order_id');
+    }
 }
