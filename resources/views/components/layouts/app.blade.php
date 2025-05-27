b291ee0a42e48dc9e4399a2ae8a4b77463fa5cfd
Kanhai Patel (TSLLP-DEV-3) <kanhaipatel@gudapps.com>
2025-05-27 13:42:06 +0530

    Added tailwindcss to header

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,6 +8,7 @@
     @livewireScripts
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div>
